75042746567b3efbe8855ff1ff493336a85a2e3a
yuhgi <yuhgi777@163.com>
2018-01-14 16:08:46 +0800

    add comment to `throttle`

--- src/throttle.js
+++ src/throttle.js
@@ -1,3 +1,13 @@
+/**
+ * @description Creates a throttled functions that only invokes `fn` at most once per 
+ * every `delay` milliseconds.`fn` will be executed at the first invoke.
+ * @param {Function} fn The functionto throttle.
+ * @param {number} delay The number of milliseconds to execute `fn`.
+ * @return {function}
+ * 
+ * @author yuhgi
+ * @version 0.0.1
+ */
 export function throttle(fn = () => {},delay = 100){
     if(typeof fn !== 'function'){
         throw new TypeError(`[${fn}] 'must be a number.`);
@@ -6,7 +16,7 @@ export function throttle(fn = () => {},delay = 100){
         throw new TypeError(`[${delay}] 'must be a positive number.`);
     }
     let timer = null,
-        last = 0;
+        last = 0;// at the first invoke,exec `fn`
     const context = this;
     return function(...args){
         const cur = +new Date();
