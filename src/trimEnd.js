c403f375744fbcc1259008de99fe52221ef854c0
yuhgi <yuhgi777@163.com>
2018-01-14 18:25:06 +0800

    method trim*

---
+++ src/trimEnd.js
@@ -0,0 +1,12 @@
+/**
+ * @description Delete the end space character of `s`.
+ * @param {String} s The string to be trim.
+ * @return {String}
+ * 
+ * @author yuhgi
+ * @version 0.0.1
+ */
+
+export default function(s){
+    return s.replace(/(\s+$)/,"");
+}
